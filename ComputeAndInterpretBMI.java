5edb2d82dabe08396cd5c5a9eae2bc0295fe1015
Alp Aziz <alp273@gwu.edu>
2020-01-20 19:04:29 -0500

    added BMI skeleton

---
+++ ComputeAndInterpretBMI.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class ComputeAndInterpretBMI {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+		// Prompt the user to enter weight in pounds
+		System.out.print("Enter weight in pounds? ");
+		double weight = input.nextDouble();
+
+		// Prompt the user to enter height in inches
+		System.out.print("Enter height in inches? ");
+		double height = input.nextDouble();
+
+		final double KILOGRAMS_PER_POUND = 0.45359237; // Constant
+		final double METERS_PER_INCH 	 = 0.0254; 	   // Constant
+
+		// Convert weight into weightInKilograms
+
+		// Convert height into heightInMeters
+
+		
+		// Compute BMI
+		
+
+		// Output BMI
+
+		// Output BMI classification
+		// USE if - else if - else
+	}
+}
