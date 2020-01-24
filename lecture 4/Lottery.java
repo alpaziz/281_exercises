2145a070428f13ee318a883984ebbb98fda8ad1d
Alp Aziz <alp273@gwu.edu>
2020-01-23 20:04:21 -0500

    added lecture 4 code

---
+++ lecture 4/Lottery.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Lottery {
+	public static void main(String[] args) {
+		// Generate a lottery number
+		// Create a double digit int
+		int lottery = ;
+
+		// Prompt the user to enter a guess
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter your lottery pick (two digits): ");
+		int guess = input.nextInt();
+
+		// Get digits from lottery
+		// Remove first digit
+		int lotteryDigit1 = ;
+
+		// Remove last digit
+		int lotteryDigit2 = ;
+
+		// Get digits from guess
+		// Remove first digit
+		int guessDigit1 = ;
+
+		// Remove last digit
+		int guessDigit2 = ;
+
+		System.out.println("The lottery number is " + lottery);
+
+		// Check the guess
+		// Create conditionals
+
+	}
+}
