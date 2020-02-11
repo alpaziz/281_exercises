105fa142334897c621c8cf95db664f2e177fab42
Alp Aziz <alp273@gwu.edu>
2020-02-10 21:13:20 -0500

    added lecture 8

---
+++ lecture 8/BinarySearch.java
@@ -0,0 +1,22 @@
+public class BinarySearch{
+	public static void main(String[] args) {
+		int[] mylist = {2, 4, 7, 10, 11, 45, 50, 59, 60, 66, 69, 70, 79};
+
+		System.out.println(binarySearch(mylist, 10));
+	}
+
+	public static int binarySearch(int[] list, int key){
+		// Declare & Assign low and high variables
+
+		int mid;
+
+		while(high >= low){
+
+			// update mid
+
+			// account for each of the three scenarios
+		}
+
+		return -low - 1; // Now high < low, key not found
+	}
+}
