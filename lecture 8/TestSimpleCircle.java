93b8f7b86d4ca0ce9500bbd4c120d6136d5c449d
Alp Aziz <alp273@gwu.edu>
2020-02-10 22:38:35 -0500

    added lecture 8 final

---
+++ lecture 8/TestSimpleCircle.java
@@ -0,0 +1,33 @@
+public class TestSimpleCircle {
+	public static void main(String[] args) {
+		
+		// Create instance of your class
+		SimpleCircle circle1 = new SimpleCircle();
+
+		System.out.println("The area of the circle of radius "
+			+ circle1.radius + " is " + circle1.getArea());
+
+
+		SimpleCircle circle2 = new SimpleCircle(25);
+
+		System.out.println("The area of the circle of radius "
+			+ circle2.radius + " is " + circle2.getArea());
+	}
+}
+
+// Define the circle class with two constructors
+class SimpleCircle {
+	double radius;
+
+	/** Construct a circle with radius 1 */
+
+
+	/** Construct a circle with a specified radius */
+	
+
+	// Return area of the circle
+
+
+	// Set a new radius
+	
+}
