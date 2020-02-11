d5369cd7364a8c1c7ad8d3275887edc5575b9c65
Alp Aziz <alp273@gwu.edu>
2020-02-10 20:06:08 -0500

    added lecture 8

---
+++ lecture 8/MultiplicationTable.java
@@ -0,0 +1,25 @@
+public class MultiplicationTable{
+	public static void main(String[] args) {
+		// Display table heading
+		System.out.println("				Multiplication Table");
+
+		// Display the number title
+		System.out.print("   ");
+
+		for (int j = 1; j<= 9; j++) {
+			System.out.print("   " + j);
+		}
+
+		System.out.println("\n---------------------------------------------------");
+
+		//Display table body
+		for () {
+			System.out.print(i + " | ");
+			for () {
+				// Display product and align properly
+					
+			}
+			System.out.println();
+		}
+	}
+}
