c70e6deb26594078bb326eeec6f5344be4b23d3f
Alp Aziz <alp273@gwu.edu>
2020-02-10 22:01:35 -0500

    added lecture 8 final

---
+++ lecture 8/GetArray.java
@@ -0,0 +1,29 @@
+public class GetArray {
+	public static void main(String[] args) {
+
+		int[][] temp = getArray();
+
+		// Using a for each with a two-dimensional array
+		for (int[] d: temp) {
+		    for (int e: d) {
+		        System.out.println(e);
+		    }
+		}
+		
+	}
+
+	public static int[][] getArray(){
+		// Enter array values
+		int[][] m = ;
+
+		// Iterate over each row
+		for (int i = 0;i < m.length; i++) {
+			// Iterate over each column
+			for(){
+				
+			}
+		}
+
+		return m;
+	}
+}
