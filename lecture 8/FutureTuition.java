25a8f50740e6b31b62fbdf61dade08194b814c19
Alp Aziz <alp273@gwu.edu>
2020-02-10 20:25:29 -0500

    added lecture 8

---
+++ lecture 8/FutureTuition.java
@@ -0,0 +1,17 @@
+
+public class FutureTuition {
+	public static void main(String[] args) {
+		double tuition 	= 10000; // Year 0
+
+		// Declare and assign year
+
+		while(tuition < ){
+			tuition = tuition * 1.07;
+
+			// Increase year
+		}
+
+		System.out.println("Your tuition will be doubled in " + year + " years");
+		System.out.printf("Tuition will be $%.2f in %1d years\n", tuition, year);
+	}
+}
