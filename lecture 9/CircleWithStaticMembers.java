6c3764a7a2407df65fb320c6c3740751073fdca5
Alp Aziz <alp273@gwu.edu>
2020-02-13 21:50:45 -0500

    added lecture 9

---
+++ lecture 9/CircleWithStaticMembers.java
@@ -0,0 +1,16 @@
+public class CircleWithStaticMembers{
+	//Instance variable
+	double radius;
+
+	/** The number of objects created */
+	// Static Variable
+
+ 	// First Constructor
+
+ 	// Second Constructor
+
+
+ 	// Static method
+
+ 	// Instance method
+ }
---
+++ lecture 9/TestCircleWithStaticMembers.java
@@ -0,0 +1,23 @@
+public class TestCircleWithStaticMembers {
+	public static void main(String[] args) {
+		System.out.println("Before creating objects");
+		System.out.println("The number of Circle objects is " + CircleWithStaticMembers.numberOfObjects);
+
+		CircleWithStaticMembers c1 = new CircleWithStaticMembers();
+
+
+		// Display c1 BEFORE c2 is created
+		System.out.println("\nAfter creating c1"); System.out.println("c1: radius (" + c1.radius +") and number of Circle objects (" + c1.numberOfObjects + ")");
+		
+		// Create c2
+		CircleWithStaticMembers c2 = new CircleWithStaticMembers(5);
+		
+		c1.radius = 9;
+
+		System.out.println("\nAfter creating c2 and modifying c1"); 
+
+		System.out.println("c1: radius (" + c1.radius + ") and number of Circle objects (" + c1.numberOfObjects + ")");
+
+	 	System.out.println("c2: radius (" + c2.radius + ") and number of Circle objects (" + c2.numberOfObjects + ")");
+	}
+}
