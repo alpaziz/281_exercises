d0d4c1cdb4e66480b785cd0eb6a3a944818e757d
Alp Aziz <alp273@gwu.edu>
2020-02-17 22:25:09 -0500

    added tv skeleton

---
+++ lecture 10/TV.java
@@ -0,0 +1,39 @@
+public class TV {
+		int channel 	= 1;
+		int volumeLevel = 1;
+		boolean on 		= false;
+
+		// Declare and Assign initial values to
+		// channel, volumeLevel, on
+
+		public TV(){
+		}
+
+
+		// methods to turn tv on & off
+
+		// method to change channel based on following condition
+		// TV has to be turned on and the inputed channel has to be between 1 and 120
+
+		// method to change volume based on following condition
+		// TV has to be turned on and the inputed volume has to be between 1 and 7
+
+	
+
+		// method to increase channel based on following condition
+		// TV has to be turned on and the current channel has to be less than 120
+	
+
+		// method to decrease channel based on following condition
+		// TV has to be turned on and the current channel has to be less than more than 1
+	
+
+
+		// method to increase volume based on following condition
+		// TV has to be turned on and the current volume has to be less than 7
+
+
+		// method to increase volume based on following condition
+		// TV has to be turned on and the current volume has to be more than 1
+
+}
